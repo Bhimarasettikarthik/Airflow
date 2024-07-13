e109deb88a63fb552b0ddc0c05b6b2c2aab10466
Bhimarasettikarthik <77921353+Bhimarasettikarthik@users.noreply.github.com>
2024-07-14 00:00:01 +0530

    Update user_processing.py

--- user_processing.py
+++ user_processing.py
@@ -1,24 +1,14 @@
 from airflow import DAG
-from datetime import datetime
-
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
-# import here
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-
+ 
 import json
 from pandas import json_normalize
-
-# The task to run
-def _store_user():
-    hook = PostgresHook(postgres_conn_id='postgres')
-    hook.copy_expert(
-        sql="COPY users FROM stdin WITH DELIMITER as ','",
-        filename='/tmp/processed_user.csv'
-    )
-
+from datetime import datetime
+ 
 def _process_user(ti):
     user = ti.xcom_pull(task_ids="extract_user")
     user = user['results'][0]
@@ -29,15 +19,18 @@ def _process_user(ti):
         'username': user['login']['username'],
         'password': user['login']['password'],
         'email': user['email'] })
-    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)  
-    
-with DAG(
-    dag_id="user_processing",
-    start_date=datetime(2023, 1, 1),
-    schedule_interval="@daily",
-    catchup=False
-) as dag:
-    
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
+ 
+def _store_user():
+    hook = PostgresHook(postgres_conn_id='postgres')
+    hook.copy_expert(
+        sql="COPY users FROM stdin WITH DELIMITER as ','",
+        filename='/tmp/processed_user.csv'
+    )
+ 
+with DAG('user_processing', start_date=datetime(2022, 1, 1), 
+        schedule_interval='@daily', catchup=False) as dag:
+ 
     create_table = PostgresOperator(
         task_id='create_table',
         postgres_conn_id='postgres',
@@ -50,14 +43,15 @@ with DAG(
                 password TEXT NOT NULL,
                 email TEXT NOT NULL
             );
-        ''')
-        
+        '''
+    )
+ 
     is_api_available = HttpSensor(
         task_id='is_api_available',
         http_conn_id='user_api',
         endpoint='api/'
     )
-    
+ 
     extract_user = SimpleHttpOperator(
         task_id='extract_user',
         http_conn_id='user_api',
@@ -66,16 +60,15 @@ with DAG(
         response_filter=lambda response: json.loads(response.text),
         log_response=True
     )
-    
+ 
     process_user = PythonOperator(
         task_id='process_user',
         python_callable=_process_user
     )
-    
-    # Add the task here
-    store_user=PythonOperator(
+ 
+    store_user = PythonOperator(
         task_id='store_user',
         python_callable=_store_user
     )
-    
-    extract_user >> process_user
+ 
+    create_table >> is_api_available >> extract_user >> process_user >> store_user
